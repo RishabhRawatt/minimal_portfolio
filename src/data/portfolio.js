b115b82ae0c0ef91caf970cb037e80dae23fef6c
Rishabh Rawat <rishabh2001rawat@gmail.com>
2023-01-18 11:37:47 +0530

    added link and img

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,21 +1,21 @@
 export default[
     {
         title: 'Faq Chatbot',
-        imgUrl: './assets/chatbot.jpg',
+        imgUrl: './assets/chat.jpg',
         stack: ['Dialogflow','Nodejs','Reactjs'],
-        link:'https://github.com/RishabhRawatt',
+        link:'https://github.com/RishabhRawatt/FAQchatBot',
     },
     {
         title: 'Encrypt and decrypt image app',
         imgUrl: './assets/encrypt.jpg',
         stack: ['AES','Android Studio','JAVA'],
-        link:'https://github.com/RishabhRawatt',
+        link:'https://github.com/RishabhRawatt/Encryt-Decrypt',
     },
     {
         title: 'Developer Portfolio',
         imgUrl: './assets/portfolio.jpg',
         stack: ['React','Tailwind','vite'],
-        link:'https://github.com/RishabhRawatt',
+        link:'https://github.com/RishabhRawatt/minimal_portfolio',
     },
 
 ]
